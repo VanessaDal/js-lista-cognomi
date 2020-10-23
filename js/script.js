091bfefff49de2a658c7b8e5e96d01a5d490a583
Vanessa D'Alessandro <leavesthatfall@gmail.com>
2020-10-23 16:05:25 +0200

    Aggiunto modo per fare lista disordinata

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
 
 var arr=["Bianchi","Rossi", "Duzioni", "Balsano", "Verdi"];
 
+
 // chiediamo il cognome
 
 var cognome = prompt("Qual è il tuo cognome?");
@@ -14,10 +15,29 @@ var cognome = prompt("Qual è il tuo cognome?");
 // aggiungi il cognome alla lista
 arr.push(cognome)
 
+//faccio una copia disordinata dell'array
+var arrCopy=[];
+
+for (i = 0; i < arr.length; i++) {
+    arrCopy.push(arr[i]);
+  }
+
+  console.log(arrCopy)
 
 // stampa la lista ordinata alfabeticamente
-arr.sort()
-document.getElementById("lista-ordinata").innerHTML="Questa è la lista ordinata alfabeticamente: "+arr;
+arr.sort();
+var a=0;
+console.log(arr)
+while (a<arr.length){
+document.getElementById("lista-ordinata").insertAdjacentHTML("beforeend","<li>" + arr[a] + "</li>" );
+a++}
 // stampiamo in che posizione si trova l'utente
 
 document.getElementById("posizione").innerHTML= "Gentile " + cognome + ", il tuo cognome si trova alla posizione numero " + (arr.indexOf(cognome)+1);
+
+//stampa lista disordinata alfabeticamente
+
+var b=0
+while (b<arrCopy.length){
+    document.getElementById("lista-disordinata").insertAdjacentHTML("beforeend","<li>" + arrCopy[b] + "</li>" );
+    b++};
